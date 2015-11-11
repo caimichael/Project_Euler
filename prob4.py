498e6af77f25ff385ff94b9be49e60611b6d7ead
caimichael <michaeldcai@gmail.com>
2015-11-11 17:24:59 -0500

    finished problem 4

--- prob4.py
+++ prob4.py
@@ -1,6 +1,22 @@
+# coding=utf-8
 #A palindromic number reads the same both ways.
 #The largest palindrome made from the product of two 2-digit numbers is 9009 = 91 × 99.
 
 #Find the largest palindrome made from the product of two 3-digit numbers.
 
+#Decrement from 999 * 999 to 999 * 998 ...
+#for each of product:
+#    invert = invert the digits
+#    if invert = original:
+#        print invert
+#        break
 
+palin_list = []
+for num1 in xrange(999,0,-1):
+    for num2 in xrange(999,0,-1):
+        product = num1*num2
+        if str(product) == str(product)[::-1]:
+            palin_list.append(product)
+
+palin_list.sort(reverse=True)
+print palin_list[0]
